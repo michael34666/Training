f8eb5ae9dd78c837e1fdee883a3d76ff296a864b
michael <michaeldadush12@gmail.com>
2025-07-07 23:34:33 -0700

    ךשדא

--- main.js
+++ main.js
@@ -1,46 +1,51 @@
 //location finder
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-
-  } 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
   
-  else {
-    document.getElementById("weather").innerText = "Geolocation is not supported by this browser.";
+    } 
+    
+    else {
+      document.getElementById("weather").innerText = "Geolocation is not supported .";
+    }
+    
   }
   
-}
-
-// Function to handle successful loc finder
- function success(position) {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      const API_KEY = '63bb193d80834d6483c143520250207';
-
-      const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${lon}`;
-
-      fetch(url)
-        .then(res => res.json())
-        .then(data => {
-          document.getElementById("weather").innerHTML = `
-            Location: ${data.location.name}<br>
-            Condition:${data.current.condition.text}<br>
-            Temperature: ${data.current.temp_c} °C
-          `;
-        })
-        .catch(() => {
-          document.getElementById("weather").innerText = "Could not load weather.";
-        });
-    }
-
-    function error() {
-      document.getElementById("weather").innerText = "Location not available.";
-    }
+  // Function to handle successful loc finder
+   function success(position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const API_KEY = '63bb193d80834d6483c143520250207';
+  
+        const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${lon}`;
+  
+        fetch(url)
+          .then(res => res.json())
+          .then(data => {
 
 
+            document.getElementById("Temperature").innerHTML = `${data.current.temp_c} °C`;
+            document.getElementById("Condition").innerHTML = `${data.current.condition.text}<br>`;
+            document.getElementById("Location").innerHTML = `${data.location.name}, ${data.location.country}`;
+            document.getElementById("Humidity").innerHTML = `${data.current.humidity} % <br> Humidity`;
+            document.getElementById("FeelsLike").innerHTML = `${data.current.feelslike_c} °C <br> Feels like `;
 
-// Initial call to get the user's location and weather
-getLocation();
-
-// Repeat every 30 seconds
-setInterval(getLocation, 30000);
+            
+            
+          })
+          .catch(() => {
+            document.getElementById("weather").innerText = "Could not load weather.";
+          });
+      }
+  
+      function error() {
+        document.getElementById("weather").innerText = "Location not available.";
+      }
+  
+  
+  
+  // Initial call to get the user's location and weather
+  getLocation();
+  
+  // Repeat every 30 seconds
+  setInterval(getLocation, 30000);
