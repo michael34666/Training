6bac5abb41feab5c6500b2c5a01d647156684396
michael <michaeldadush12@gmail.com>
2025-07-10 01:44:40 -0700

    add file

---
+++ second/App.ts
@@ -0,0 +1,9 @@
+
+function add(arg1:number,arg2:number) {
+  return arg1 + arg2;
+}
+const a1=2;
+const a2=3;
+
+const r1=add(a1,a2);
+console.log(r1); 
---
+++ second/temp.js
@@ -0,0 +1,54 @@
+/* import{ useState } from 'react';
+
+//
+function Sentence({text,onclick}) {
+  return (
+    <button onClick={onclick}>
+      {text}
+    </button>
+  );
+}
+
+
+/*if(addOrNot){
+   const newList=list.concat({});
+   setlist(newList);
+  }
+
+  return null;
+  */
+/*
+function TodolistSwitch({x, uncompTasks}) {
+  switch(x)
+  {
+    case add:
+      return null;
+      uncompTasks++;
+    case remove:
+      return null;
+      uncompTasks--;
+    case completed:
+      return null;
+      uncompTasks--;
+    default:
+      return null;
+    
+  }
+  
+}*/
+
+/*
+export default function MyApp() {
+  const [addOrNot, setAddOrNot] = useState(true);
+  const [uncompTasks, setUncompTasks] = useState(0);
+
+  const intialList = []
+  const[list,setlist] = useState(intialList);
+  return (
+    <div>
+      <Todolist addOrNot={addOrNot} />
+    </div>
+
+  );
+}
+*/
