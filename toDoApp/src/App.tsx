e6d6e9f8c332a936aa4ba30ea26c3b627051b380
michael <michaeldadush12@gmail.com>
2025-07-13 13:36:02 +0300

    add task

--- toDoApp/src/App.tsx
+++ toDoApp/src/App.tsx
@@ -13,16 +13,20 @@ type ToDoProps = {
 function App() {
   
   const [ToDoList, setTodoList] = useState<ToDoProps[]>([]);
+  const [newTodo, setNewTodo] = useState('');
   
 
-  function handleAdd(props: ToDoProps)
+  function handleAdd()
   {
-
+    if(newTodo.trim() === '') {
+      alert('Enter a valid task');
+      return;
+    }
+    
     setTodoList((prevList: ToDoProps[])=>{
-      
-      return[...prevList,props];
-      
+      return[...prevList,{ index: prevList.length, task: newTodo, completed: false }];
     })
+    setNewTodo(''); 
   }
 
   function handleDeleteTODO(index:number)
@@ -30,44 +34,39 @@ function App() {
     setTodoList((prevList: ToDoProps[])=>prevList.filter((task)=> task.index !== index));
   }
 
+  function print(list: ToDoProps[])
+  {
+    return list.map((task) => (
+      <div key={task.index}>
+        <span>{task.index+1}. {task.task}</span>
+      </div>
+    ));
+  }
   return (
     <>
-      <div>
-       
-        
-      </div>
       <h1>To Do App</h1>
       
-      <div className="card">
-      
-        
-        <button onClick={()=>handleAdd({index: ToDoList.length, task: 'New Task', completed: false})}>Add New Task</button>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(mes) => setNewTodo(mes.target.value)}
+          placeholder="Write your next task"/>
+
         <br></br>
-        <button onClick={()=>handleDeleteTODO(ToDoList.length - 1)}>Delete Task</button>
+        <button onClick={() => handleAdd()}>Add New Task:</button>
         <br></br>
+        <button onClick={() => handleDeleteTODO(ToDoList.length - 1)}>Delete Task</button>
+        <br></br>
+        {print(ToDoList)}
         <div>You have {ToDoList.length} tasks that are open</div>
+        <div>
+          
+        </div>
       </div>
-      <br></br>
-        <form className="form" onSubmit={handleSubmit}>
-          <label htmlFor="todo">
-            <input
-              type="text"
-              name="todo"
-              id="todo"
-              placeholder="Write your next task"
-            />
-            
-          </label>
-          <button>
-            <span className="visually-hidden">Submit</span>
-            <svg>
-              <path d="" />
-            </svg>
-          </button>
-          </form>
-      
     
-    </>
+       
+      </>
   )
 }
 
