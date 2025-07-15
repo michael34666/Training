b0f009c1da8255598ebfd793e9c00a5f73ed5936
michael <michaeldadush12@gmail.com>
2025-07-15 17:32:09 +0300

    hendle with undo/complete bug

--- toDoApp/src/App.tsx
+++ toDoApp/src/App.tsx
@@ -6,9 +6,7 @@ import './App.css'
 import handleAdd from './component/handleAdd';
 import handleDeleteTODO from './component/handleDeleteTODO';
 import handlecompleteToDo from './component/handlecompleteToDo';
-import howmanyneedTODO from './component/howmanyneedTODO';
-
-*/
+import howmanyneedTODO from './component/howmanyneedTODO';*/
 
 type ToDoProps = {
   index: number;
@@ -20,9 +18,22 @@ type ToDoProps = {
 
 
 function App() {
-  const [ToDoList, setTodoList] = useState<ToDoProps[]>([]);
+
   const [newTodo, setNewTodo] = useState('');
   
+  const [ToDoList, setTodoList] = useState(() => {
+    const toDoString = localStorage.getItem("ToDoList");
+    const storedToDo = toDoString ? JSON.parse(toDoString) : [];
+    return storedToDo || [];
+  });
+
+
+  //synchroniz the componenets- allow to display data that write in another localhost
+  useEffect(() =>{
+    localStorage.setItem('ToDoList',JSON.stringify(ToDoList))}
+  ,[ToDoList]);
+  
+ 
   function handleAdd()
   {
     if(newTodo.trim() === '') {
@@ -30,16 +41,16 @@ function App() {
       return;
     }
     
-    setTodoList((prevList: ToDoProps[])=>{
-      return[...prevList,{ index: prevList.length, task: newTodo, completed: false }];
+    setTodoList((ToDoList: ToDoProps[])=>{
+      return[...ToDoList,{ index: ToDoList.length, task: newTodo, completed: false }];
     })
     setNewTodo(''); 
   }
 
    function handlecompleteToDo(index:number)
   {
-    setTodoList((prevList: ToDoProps[]) =>
-      prevList.map((task) =>
+    setTodoList((ToDoList: ToDoProps[]) =>
+      ToDoList.map((task) =>
        task.index===index ? { ...task, completed: !task.completed } : task
      )
     );
@@ -47,7 +58,7 @@ function App() {
   }
    function handleDeleteTODO(index:number)
   {
-    setTodoList((prevList: ToDoProps[])=>prevList.filter((task)=> task.index !== index));
+    setTodoList((ToDoList: ToDoProps[])=>ToDoList.filter((task)=> task.index !== index));
   }
 
     function howmanyneedTODO()
@@ -56,7 +67,7 @@ function App() {
       return 0;
     }
     let count = 0;
-    ToDoList.forEach((task) => {
+    ToDoList.forEach((task: ToDoProps) => {
       if(!task.completed){
         count++;
       }
@@ -66,7 +77,6 @@ function App() {
 
   return (
     <>
-      
       <h1>To Do App</h1>
       
       <div>You have {howmanyneedTODO()} tasks that are open</div>
@@ -85,9 +95,10 @@ function App() {
         <div className="scroller input-container">
        
         <ul>
-          { ToDoList.map((task) => (
+          { ToDoList.map((task: ToDoProps) => (
           <li key={task.index}>
             <span className={task.completed ? 'strikethrough' : ''}>{task.task}</span>
+            <br></br>
             <button onClick={() => handleDeleteTODO(task.index)}>Delete Task</button>
             <button onClick={() => handlecompleteToDo(task.index)}>
               {task.completed ? 'Undo' : 'Complete'}
@@ -106,4 +117,3 @@ function App() {
 }
 
 export default App
-
