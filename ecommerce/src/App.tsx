b8df4c28d548066e606a1650293911eaf467369e
michael <michaeldadush12@gmail.com>
2025-07-20 18:07:13 +0300

    fix the remark

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -3,11 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <h1>Ecommerce App</h1>
+    </>
+   
+  )
+}
+
+export default App
+/*
+ <>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +30,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Ecommerce App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +43,4 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
-
-export default App
+    */
