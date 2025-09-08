2f05b7632efde543a260b5ca065a6bdf36523f3f
michael <michaeldadush12@gmail.com>
2025-09-08 16:54:58 +0300

    add swagger

--- Training/ecommerce-backend-part2/apps/gateway/src/main.ts
+++ Training/ecommerce-backend-part2/apps/gateway/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { configDotenv } from 'dotenv';
 import 'dotenv/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 configDotenv({
   path: resolve(__dirname, '../../../.env'),
@@ -13,6 +14,14 @@ async function bootstrap() {
   const DEFAULT_PORT = 3000;
 
   const app = await NestFactory.create(AppModule);
+   const config = new DocumentBuilder()
+    .setTitle('gateway')
+    .setDescription('The gateway API description')
+    .setVersion('1.0')
+    .addTag('gateway')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app
     .listen(process.env.GATEWAY_SERVICE_PORT ?? DEFAULT_PORT)
